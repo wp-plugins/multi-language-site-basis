35bbed1dca2e2d1b5359a4e87b6dc109ee5626b9
selnomeria <selnomeria@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-14 20:30:06 +0000

    update
    git-svn-id: https://plugins.svn.wordpress.org/multi-language-site-basis/trunk@1134885 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- MultiLangSite.php
+++ MultiLangSite.php
@@ -48,7 +48,7 @@ register_activation_hook( __FILE__, 'activation__MLSS' );function activation__ML
 		//
 		update_option('optMLSS__DropdHeader','y'); update_option('optMLSS__DropdSidePos','left'); update_option('optMLSS__DropdDistanceTop','70');update_option('optMLSS__DropdDistanceSide','50');
 		//
-		update_option('optMLSS__CategSlugname',	'categories');   update_option('optMLSS__PageSlugname', '');
+		update_option('optMLSS__CategSlugname',	'');   update_option('optMLSS__PageSlugname', '');
 		
 	}
 	
