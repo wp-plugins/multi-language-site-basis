458d85ae819fc381f9de4ba5148ba677a5c7f135
selnomeria <selnomeria@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-25 15:27:06 +0000

    minor update
    git-svn-id: https://plugins.svn.wordpress.org/multi-language-site-basis/trunk@1167189 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- flags/ip_country_detect/sample_test.php
+++ flags/ip_country_detect/sample_test.php
@@ -1,6 +1,6 @@
 <?php
 $user_ippp = $_SERVER['REMOTE_ADDR'];
-if (!class_exists("GeoIP")){ include_once( __DIR__ ."/geoip.inc"); }
+if (!class_exists('GeoIP')){include_once( dirname(__file__)."/geoip.inc" );}
 $gi = geoip_open( __DIR__ ."/GeoIP.dat", GEOIP_STANDARD); 
 $country_name = geoip_country_name_by_addr($gi, $user_ippp );
 geoip_close($gi);
